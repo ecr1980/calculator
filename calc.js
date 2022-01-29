f052755835d6deeceaa15a15537b83eefcfe4db8
Eric Romano <ecr1980@gmail.com>
2022-01-29 01:44:38 -0500

    Added button and display functionality. Added a math function. Current version correctly displays numbers on screen when using on screen buttons. Clear and Clear everything buttons work as intended. Math works almost as intended. Math buttons work almost as intended. 2 major bugs showing at this time. 1st, hitting one of the math function buttons after hitting the enter button automatically performs that function with the previous entered value. 2nd bug, 5/4 = infinity. How to handle large decimals is an issue that will need to be solved.

--- calc.js
+++ calc.js
@@ -1,1 +1,118 @@
-document.getElementById('display').innerHTML = '0';
+
+
+
+let display = '';
+let action = '';
+let lastValue = 0;
+let sideValue = 0;
+
+
+
+for (let i = 0; i< 10; i++){
+    const buttonAssign = document.getElementById(`button${i}`);
+    buttonAssign.addEventListener('click',function(){
+        valueInput(buttonAssign.innerHTML);
+    });
+}
+
+const addAssign = document.getElementById('buttonAdd');
+addAssign.addEventListener('click', function(){
+   
+    calculate();
+    action = '+';
+    display = '';
+    document.getElementById('display').innerHTML = lastValue;
+    
+})
+
+const subtractAssign = document.getElementById('buttonSubtract');
+subtractAssign.addEventListener('click', function(){
+  
+    calculate();
+    action = '-';
+    display = '';
+    document.getElementById('display').innerHTML = lastValue;
+    
+})
+
+const multiplyAssign = document.getElementById('buttonMultiply');
+multiplyAssign.addEventListener('click', function(){
+    
+    calculate();
+    action = '*';
+    display = '';
+    document.getElementById('display').innerHTML = lastValue;
+    
+    
+})
+
+const divideAssign = document.getElementById('buttonDivide');
+divideAssign.addEventListener('click', function(){
+
+    calculate();
+    action = '/';
+    display = '';
+    document.getElementById('display').innerHTML = lastValue;
+    
+})
+
+const plusMinusAssign = document.getElementById('buttonPlusMinus');
+plusMinusAssign.addEventListener('click', function(){
+    if (display.charAt(0) == '-'){
+        display = display.slice(1);
+    }
+    else{
+        display = '-' + display;
+    }
+    document.getElementById('display').innerHTML = display;
+})
+
+const periodAssign = document.getElementById('buttonPeriod');
+periodAssign.addEventListener('click', function(){
+    display = display + '.';
+    document.getElementById('display').innerHTML = display;
+})
+
+const equalsAssign = document.getElementById('buttonEquals');
+equalsAssign.addEventListener('click', function(){
+    calculate();
+    //display = '';
+    document.getElementById('display').innerHTML = lastValue;
+})
+
+const clearAssign = document.getElementById('buttonClear');
+clearAssign.addEventListener('click', function(){
+    display = '';
+    document.getElementById('display').innerHTML = display;
+})
+
+const clearEverythingAssign = document.getElementById('buttonClearEverything');
+clearEverythingAssign.addEventListener('click', function(){
+    display = '';
+    action = '';
+    lastValue = 0;
+    document.getElementById('display').innerHTML = display;
+})
+
+function valueInput(x){
+    display = display + x;
+    document.getElementById('display').innerHTML = display;
+}
+
+function calculate(){
+    if(action == ''){
+        lastValue = display;
+    }
+    else if(action == '+'){
+        lastValue = +lastValue + +display; 
+    }
+    else if(action == '-'){
+        lastValue = +lastValue - +display; 
+    }
+    else if(action == '*'){
+        lastValue = +lastValue * +display; 
+    }
+    else{
+        lastValue = +lastValue / +display; 
+    }
+}
